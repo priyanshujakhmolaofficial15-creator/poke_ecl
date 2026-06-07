0523b08a887a302f8edb3269c81dfc40b332831c
priyanshujakhmolaofficial15-creator <priyanshujakhmolaofficial15@gmail.com>
2026-06-07 16:40:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,9 @@
-import os
-from dotenv import load_dotenv
-
-
-load_dotenv()
-
-
-API_ID: int = int(os.getenv('api_id'))
-API_HASH: str = os.getenv('api_hash')
-SESSION_STRING:str = os.getenv('string_session',None)
+API_ID: int = int(25565596)
+API_HASH: str = "7d7b960c2eba7994824891a084a434d6"
+SESSION_STRING:str = "BQGGGZwAgpzx2K5MBjAyP0beD0nDDG7WWDL201_0aG7ADF3nwL-te4kBMuYwLH6IWLmj5Tom0K-1AMMBsLtM37WwO4RHifK1ayMlfLzpP6yFLsQB3ghtPuelyB3qYXJu4nRFB4ixDwYtcTQ_KJiKAu4RNNU-atamdzab1JgLdhLer-MOTSJ6SBnw9iMi0GomIvFQPy_fFLOlsUIQpKekzBwgG5URIlYLLFctoWOlfROuH05eGUQOZKGf7YSTIFDAmxaRzv4lyupYSwubyLiLjXgLGjJDdqZEf0GL_JpAo7_B1YYt-XdfpaPSdPyFukzrPfHq2voy8NNgpiC1unP2qskURkWu5AAAAABWRAmmAA"
 PREFIXES: list[str] = [".", "@", "#", "$", "%", "^", "&", "*", "~"]
 BOT_USR:str = "PokeEclipseXBot"
-GC_ID:int = int(os.getenv('gc_id'))
+GC_ID:int = int(-1002891260712)
 
 __all__ = [
     "API_ID",
